e9008f99dd419cc00740940425addf430704f1ba
mustafaferhatimamoglu <mustafaferhatimamoglu@gmail.com>
2023-12-19 00:52:40 +0300

    2

--- telegram/t2.py
+++ telegram/t2.py
@@ -11,3 +11,18 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+    
+    
+    
+import asyncio
+import telegram
+
+
+async def main():
+    bot = telegram.Bot("6474533873:AAEUK4AtkTfJr3eghfSGM1Y_4di_vW7kIkI")
+    async with bot:
+        print(await bot.get_me())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
